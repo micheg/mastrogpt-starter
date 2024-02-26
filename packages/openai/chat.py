a074c51d2674aa5093de7b323fa3355db5e7166b
Michelangelo Giacomelli <michelangelog@gmail.com>
2024-02-26 10:14:40 +0000

    stop on 404 on chess.

--- packages/openai/chat.py
+++ packages/openai/chat.py
@@ -4,6 +4,9 @@
 
 from openai import AzureOpenAI
 import re
+import requests
+import socket
+
 
 ROLE = """
 When requested to write code, pick Python.
@@ -64,6 +67,46 @@ def extract(text):
         return res
     return res
 
+# custom
+
+def slack_log(_str):
+    _uri = 'https://nuvolaris.dev/api/v1/web/utils/demo/slack\?text={TEXT}'
+    r = requests.get(_uri.replace('{TEXT}', _str))
+    print('sending slack message')
+    return r
+
+def check_if_is_mail(_str):
+    _finded = False
+    _pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    if(re.fullmatch(_pattern, _str)):
+        r = slack_log(f'Hello from {_str}!')
+        _finded = True
+    return _finded
+
+def extract_domains(_str):
+    _pattern = r"(?<!\w)([a-z0-9-]{5,}\.[a-z]{2,6})(?!\w)"
+    matches = re.finditer(_pattern, _str)
+    domains = [match.group(1) for match in matches]
+    if len(domains) <1: return None
+    return domains
+
+def get_ip_by_name(domain):
+    try:
+        ip = socket.gethostbyname(domain)
+        return ip
+    except socket.herror:
+        return None
+
+def find_word(_word):
+    return re.compile(r'\b({0})\b'.format(_word), flags=re.IGNORECASE).search
+
+def get_chess_puzzle():
+    print("aaa")
+    endpoint = 'https://pychess.run.goorm.io/api/puzzle?limit=1'
+    r = requests.get(endpoint)
+    if r.status_code == 200:
+        pass
+
 def main(args):
     global AI
     (key, host) = (args["OPENAI_API_KEY"], args["OPENAI_API_HOST"])
@@ -74,11 +117,40 @@ def main(args):
         res = {
             "output": "Welcome to the OpenAI demo chat",
             "title": "OpenAI Chat",
-            "message": "You can chat with OpenAI."
+            "message": "You can chat with OpenAI. (TEST)"
         }
     else:
+        # user has put just an email, no need for ai
+        if check_if_is_mail(input):
+            res['output'] = 'sended to slack'
+            return {"body": res }
+        
+        # check for domain
+        _domains = extract_domains(input)
+        if _domains is not None:
+            ip = get_ip_by_name(_domains[0])
+            if ip is None:
+                res['output'] = 'unable to get ip'
+                return {"body": res }
+            # add more prompt
+            tmp = f'Assuming {_domains[0]} has IP address {ip}, answer to this question: ' + input
+            slack_log(f'user request ip for {_domains[0]}')
+            output = ask(tmp)
+            res = extract(output)
+            res['output'] = output
+            return {"body": res }
+        
+        # check for chess    
+        if (find_word('chess')(input) is not None or find_word('scacchi')(input) is not None):
+            tmp = f'is the following a request for a chess puzzle: "{input}": Answer Yes or No'
+            output = ask(tmp)
+            if output.lower() == 'yes':
+                _puzzle_data = get_chess_puzzle()                
+            res = extract(output)
+            res['output'] = output
+            return {"body": res }
+        
         output = ask(input)
         res = extract(output)
         res['output'] = output
-
     return {"body": res }
